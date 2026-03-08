19398209aa425174a3768403a42a42ed759ff641
Pritthish20 <pritthish.sarkar@sportsdunia.com>
2026-03-09 02:30:30 +0530

    controller added

--- src/controller/identify.controller.ts
+++ src/controller/identify.controller.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import { processContacts } from "../services/identify.service";
 
-export const identifyUser= (req:Request,res:Response)=>{
-    
+export const identifyUser=async (req:Request,res:Response)=>{
+
+    try {
+        const contact= await processContacts();
+
+        return res.status(200).json({
+            data: contact,
+            message: "User data processed successfully"
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            error:error,
+            message: "Internal server error"
+        })
+    }
 }
